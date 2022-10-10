4606c2df974aa70426b6a697701a2bf419ff3ec1
cmd430 <cmd430@gmail.com>
2022-10-11 03:04:49 +1300

    inital commit

---
+++ dist/user.js
@@ -0,0 +1,18 @@
+// Prefs to unfuck the experience
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.urlbar.openViewOnFocus", false);
+user_pref("browser.urlbar.suggest.bookmark", false);
+user_pref("browser.urlbar.suggest.openpage", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.urlbar.suggest.quickactions", false);
+user_pref("browser.compactmode.show", true);
+user_pref("alerts.useSystemBackend", true);
+//userChrome Prefs
+user_pref("userChrome.BetterFindBar", true);
+user_pref("userChrome.UltraCompact", false);
+user_pref("userChrome.ColoredTabMediaControls", true);
+user_pref("userChrome.RoundedBackButton", true);
+user_pref("userChrome.SmartBookmarkBar", true);
+user_pref("userChrome.CompactMenus", true);
+user_pref("userChrome.SaferPromptDialogs", true);
+user_pref("userChrome.RemoveAllTabsButton", true);
