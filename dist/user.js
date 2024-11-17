376a9f7edf0cd4eceb2c7aa7b7966901abf239c1
cmd430 <cmd430@gmail.com>
2024-11-17 22:58:02 +1300

    fix menu icon colors for normal firefox

--- dist/user.js
+++ dist/user.js
@@ -1,5 +1,6 @@
 // Prefs to unfuck the experience
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("svg.context-properties.content.enabled", true);
 user_pref("browser.urlbar.openViewOnFocus", false);
 user_pref("browser.urlbar.suggest.addons", false);
 user_pref("browser.urlbar.suggest.bookmark", false);
