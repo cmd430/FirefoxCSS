eca790154688a51df86e2fcaf3568247033b450c
cmd430 <cmd430@gmail.com>
2024-11-22 02:03:37 +1300

    compile from scr to dist

---
+++ src/user.js
@@ -0,0 +1,41 @@
+// Prefs to unfuck the experience
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("browser.urlbar.openViewOnFocus", false);
+user_pref("browser.urlbar.suggest.addons", false);
+user_pref("browser.urlbar.suggest.bookmark", false);
+user_pref("browser.urlbar.suggest.calculator", false);
+user_pref("browser.urlbar.suggest.clipboard", false);
+user_pref("browser.urlbar.suggest.engines", false);
+user_pref("browser.urlbar.suggest.fakespot", false);
+user_pref("browser.urlbar.suggest.mdn", false);
+user_pref("browser.urlbar.suggest.openpage", false);
+user_pref("browser.urlbar.suggest.pocket", false);
+user_pref("browser.urlbar.suggest.quickactions", false);
+user_pref("browser.urlbar.suggest.recentsearches", false);
+user_pref("browser.urlbar.suggest.remotetab", false);
+user_pref("browser.urlbar.suggest.searches", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.urlbar.suggest.trending", false);
+user_pref("browser.urlbar.suggest.weather", false);
+user_pref("browser.urlbar.suggest.yelp", false);
+user_pref("browser.urlbar.suggest.history", true);
+user_pref("browser.compactmode.show", true);
+user_pref("alerts.useSystemBackend", true);
+user_pref("extensions.unifiedExtensions.enabled", false);
+user_pref("signon.firefoxRelay.feature", "disabled");
+user_pref("dom.ipc.processPriorityManager.backgroundUsesEcoQoS", false);
+user_pref("widget.windows.window_occlusion_tracking.enabled", false);
+user_pref("browser.tabs.hoverPreview.enabled", false);
+
+
+//userChrome Prefs
+user_pref("userChrome.BetterFindBar", true);
+user_pref("userChrome.UltraCompact", true);
+user_pref("userChrome.ColoredTabMediaControls", true);
+user_pref("userChrome.RoundedBackButton", true);
+user_pref("userChrome.SmartBookmarkBar", true);
+user_pref("userChrome.CompactMenus", true);
+user_pref("userChrome.SaferPromptDialogs", true);
+user_pref("userChrome.RemoveAllTabsButton", true);
+user_pref("userChrome.DisableInactiveFadeout", true);
