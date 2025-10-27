6bc3ebfbf5b7ad116bae9f3f150d0c8c07f1bf79
cmd430 <cmd430@gmail.com>
2025-10-27 18:37:45 +1300

    more ai disable

--- src/user.js
+++ src/user.js
@@ -9,6 +9,7 @@ user_pref("dom.ipc.processPriorityManager.backgroundUsesEcoQoS", false);
 user_pref("media.suspend-background-video.enabled", false);
 user_pref("browser.tabs.hoverPreview.enabled", false);
 user_pref("browser.tabs.groups.enabled", false);
+user_pref("browser.tabs.groups.smart.enabled", false);
 user_pref("browser.urlbar.suggest.addons", false);
 user_pref("browser.urlbar.suggest.bookmark", false);
 user_pref("browser.urlbar.suggest.calculator", false);
